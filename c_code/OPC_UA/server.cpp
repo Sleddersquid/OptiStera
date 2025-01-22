2fab3f15915dae60ed3ff6ce3e19085c459fd3ea
ErikSNyborg <esnv96@gmail.com>
2025-01-22 13:38:03 +0000

    OPC UA with c++ and c. Added index file for website

--- c_code/OPC_UA/server.cpp
+++ c_code/OPC_UA/server.cpp
@@ -1,11 +1,13 @@
-#include <iostream>
-
-
-
+#include <open62541pp/open62541pp.hpp>
 
 int main() {
+    opcua::Server server;
 
+    // Add a variable node to the Objects node
+    opcua::Node parentNode(server, opcua::ObjectId::ObjectsFolder);
+    opcua::Node myIntegerNode = parentNode.addVariable({1, 1000}, "TheAnswer");
+    // Write value attribute
+    myIntegerNode.writeValueScalar(42);
 
-  std::cout << "End of file" << std::endl;
-  return 0;
+    server.run();
 }
