19ccdda3a495ef7bb11220f8527a2e692a80773d
ErikSNyborg <esnv96@gmail.com>
2025-03-05 18:27:23 +0100

    Tweaked padding for the sidebar on mobile, easier to access the different pages. Javascript encapsulated in re-size event listener function. Fixed Js format (two indents => one indent)

--- updates/script.js
+++ updates/script.js
@@ -1,15 +1,16 @@
-// Kanksje sette en if så at denne ikke langes når menyen ikke er ute
-if (screen.width < 801) {
-    function showSidebar() {
-        const sidebarEl = document.querySelector("ul.sidebar");
-        sidebarEl.style.display = "flex";
-    }
-    function hideSidebar() {
-        const sidebarEl = document.querySelector("ul.sidebar");
-        sidebarEl.style.display = "none";
-    }
+function onResize() {
+    if (screen.width < 801) {
+        function showSidebar() {
+            const sidebarEl = document.querySelector("ul.sidebar");
+            sidebarEl.style.display = "flex";
+        }
+        function hideSidebar() {
+            const sidebarEl = document.querySelector("ul.sidebar");
+            sidebarEl.style.display = "none";
+        }
+    };
+}
 
+window.addEventListener('resize', onResize);
 
-
-    
-};
+onResize(); // For first page load
