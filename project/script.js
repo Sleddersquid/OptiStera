3e87a8bad5a7e05fa8ced45c74f9b72b3abe50be
ErikSNyborg <esnv96@gmail.com>
2025-03-05 13:11:51 +0100

    Many changes

--- project/script.js
+++ project/script.js
@@ -1,11 +1,22 @@
 // Kanksje sette en if så at denne ikke langes når menyen ikke er ute
-if (screen.width < 801 || true) {
-    function showSidebar() {
-        const sidebarEl = document.querySelector("ul.sidebar");
-        sidebarEl.style.display = "flex";
-    }
-    function hideSidebar() {
-        const sidebarEl = document.querySelector("ul.sidebar");
-        sidebarEl.style.display = "none";
-    }
-};
+if (screen.width < 801) {
+    console.log(screen.width);
+      function showSidebar() {
+          const sidebarEl = document.querySelector("ul.sidebar");
+          sidebarEl.style.display = "flex";
+      }
+      function hideSidebar() {
+          const sidebarEl = document.querySelector("ul.sidebar");
+          sidebarEl.style.display = "none";
+      }
+  
+      // CHATGPT
+      document.querySelectorAll("h2.heading").forEach((heading) => {
+          const span = heading.querySelector("span.line-title.left");
+        
+          if (span) {
+            span.classList.replace("left", "right"); // Change class from "left" to "right"
+            heading.appendChild(span); // Move <span> to the end of <h2>
+          }
+        });
+  };
