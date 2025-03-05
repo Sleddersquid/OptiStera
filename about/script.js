23315a0dc691bb63c8cc02894c989f609d9d75b9
ErikSNyborg <esnv96@gmail.com>
2025-03-05 18:49:48 +0100

    Fixed socping of Js function show/hide side bar. Fixed hover on landing page timeline to apply only when screen width is bigger than 800px. Removed resize in about and updates, no need. Added back padding in navbar-acnhor (ops).

--- about/script.js
+++ about/script.js
@@ -1,16 +1,11 @@
-function onResize() {
-    if (screen.width < 801 || true) {
-        function showSidebar() {
-            const sidebarEl = document.querySelector("ul.sidebar");
-            sidebarEl.style.display = "flex";
-        }
-        function hideSidebar() {
-            const sidebarEl = document.querySelector("ul.sidebar");
-            sidebarEl.style.display = "none";
-        }
-    };
-}
-
-window.addEventListener('resize', onResize);
-
-onResize(); // For first page load
+if (screen.width < 801 || true) {
+    console.log(screen.width);
+    function showSidebar() {
+        const sidebarEl = document.querySelector("ul.sidebar");
+        sidebarEl.style.display = "flex";
+    }
+    function hideSidebar() {
+        const sidebarEl = document.querySelector("ul.sidebar");
+        sidebarEl.style.display = "none";
+    }
+};
--- script.js
+++ script.js
@@ -1,15 +1,15 @@
+function showSidebar() {
+    const sidebarEl = document.querySelector("ul.sidebar");
+    sidebarEl.style.display = "flex";
+}
+function hideSidebar() {
+    const sidebarEl = document.querySelector("ul.sidebar");
+    sidebarEl.style.display = "none";
+}
 function onResize() {
   if (screen.width < 801) {
     console.log(screen.width);
-      function showSidebar() {
-          const sidebarEl = document.querySelector("ul.sidebar");
-          sidebarEl.style.display = "flex";
-      }
-      function hideSidebar() {
-          const sidebarEl = document.querySelector("ul.sidebar");
-          sidebarEl.style.display = "none";
-      }
-  
+
       // CHATGPT
       document.querySelectorAll("h2.heading").forEach((heading) => {
           const span = heading.querySelector("span.line-title.left");
